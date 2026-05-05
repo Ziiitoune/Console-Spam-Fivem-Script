3f1e242730db751878183c1e1c768a9a07bdd0ce
Ziiitoune <officiel.zitoune@gmail.com>
2026-05-05 14:41:14 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zitoune'
+
+client_scripts {
+    'main.lua'
+}
---
+++ main.lua
@@ -0,0 +1,8 @@
+local link = "discord.gg/zitoune"
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        print(link)
+    end
+end)
